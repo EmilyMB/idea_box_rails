967f807b44948f744821d9c11309feaafbfadb00
Emily Berkeley <eberkeley@gmail.com>
2015-01-15 14:59:40 -0700

    Basic authorization working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+  before_filter :authorize, only: [:show]
+
   def show
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to not_found_path
+    end
   end
 end
